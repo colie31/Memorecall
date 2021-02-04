47112cedcb737cc3cb6af3adb8c998fd6d6bf23b
colie31 <colie4272@gmail.com>
2021-02-03 19:40:00 -0500

    imported database schema

--- app/models/categories.py
+++ app/models/categories.py
@@ -7,3 +7,11 @@ class Category(db.Model, UserMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(25), nullable=False)
+
+    
+
+    def to_dict(self):
+    return {
+      "id": self.id,
+      "name": self.name,
+    }
