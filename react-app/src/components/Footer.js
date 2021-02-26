130c330bcb90623f10ac2343db45c7d94f525c41
colie31 <colie4272@gmail.com>
2021-02-26 10:35:40 -0500

    updated footer with info

--- react-app/src/components/Footer.js
+++ react-app/src/components/Footer.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom"
 
 const Footer = () => {
     return (
-        <footer>
-          <h1>I am a Footer</h1>
-        </footer>
-    )
+      <footer className="footer">
+        <h1>Nichole O'Brien</h1>
+        <a href="https://github.com/colie31" alt="">
+          GitHub
+        </a>
+        <a href="https://www.linkedin.com/in/nichole-o-brien-38124a1a3/" alt="">
+          Linkedin
+        </a>
+      </footer>
+    );
 }
 
 export default Footer;
--- react-app/src/pics/Journal.jsx
+++ react-app/src/pics/Journal.jsx
@@ -10,7 +10,7 @@ const Journal  = ({ journal, func, dispatch, setJournal }) => {
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
     >
-      <title>something</title>
+      <title>{journal.name}</title>
       <g
         id="Page-1"
         stroke="none"
