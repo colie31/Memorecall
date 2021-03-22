633a3657905cae6c11c8fa00f1f0a894d96ec98b
colie31 <colie4272@gmail.com>
2021-03-21 21:38:16 -0400

    updated seeder files

--- app/seeds/entries.py
+++ app/seeds/entries.py
@@ -7,17 +7,18 @@ def seed_entries():
     journal = Journal.query.first()
     category = Category.query.first()
 
-    demo = Entry(
-        body = 'My first journal entry, How remarkable',
-        date = datetime.datetime.now(),
-        page_layout = 'layout_two',
-        user_id = user.id,
-        journal_id = journal.id,
-        category_id = category.id
-    )
+    # Maybe implement python based AWS logic for adding pics
+    # demo = Entry(
+    #     body = 'My first journal entry, How remarkable',
+    #     date = datetime.datetime.now(),
+    #     page_layout = 'layout_two',
+    #     user_id = user.id,
+    #     journal_id = journal.id,
+    #     category_id = category.id
+    # )
 
-    db.session.add(demo)
-    db.session.commit()
+    # db.session.add(demo)
+    # db.session.commit()
 
     paragraph = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget mauris pharetra et ultrices neque ornare aenean euismod elementum. Tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin. Et odio pellentesque diam volutpat. Adipiscing elit ut aliquam purus sit amet luctus venenatis. Pellentesque habitant morbi tristique senectus et. Aliquam ultrices sagittis orci a scelerisque purus semper eget duis. Vitae nunc sed velit dignissim sodales ut eu sem integer. Donec ac odio tempor orci. Erat nam at lectus urna duis convallis convallis. Eu non diam phasellus vestibulum. Tempor nec feugiat nisl pretium fusce id velit ut. Nam at lectus urna duis convallis convallis tellus id interdum. Sed euismod nisi porta lorem mollis aliquam ut. Sed ullamcorper morbi tincidunt ornare. Interdum posuere lorem ipsum dolor sit amet. Mauris rhoncus aenean vel elit scelerisque mauris pellentesque. Senectus et netus et malesuada fames. Neque sodales ut etiam sit amet nisl purus in. Eget felis eget nunc lobortis mattis aliquam faucibus purus.'
 
--- app/seeds/journals.py
+++ app/seeds/journals.py
@@ -1,4 +1,8 @@
 from app.models import db, User, Journal
+import random
+# Generating random number number to contribute in creating
+# a hex code to store in the database line 21
+r = lambda: random.randint(0, 255)
 
 def seed_journals():
     user = User.query.first()
@@ -10,6 +14,14 @@ def seed_journals():
     )
 
     db.session.add(demo)
+
+    for num in range(2, 18):
+        demo2 = Journal(
+            name = f'Journal {num}',
+            color = '#{:02x}{:02x}{:02x}'.format(r(), r(), r()),
+            user_id = user.id
+        )
+        db.session.add(demo2)
 
     db.session.commit()
 
