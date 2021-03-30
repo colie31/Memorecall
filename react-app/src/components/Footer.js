454315c5991fc8b7f958f66bdd3413e1981319c9
colie31 <colie4272@gmail.com>
2021-03-30 15:22:16 -0400

    updated footer

--- react-app/src/components/Footer.js
+++ react-app/src/components/Footer.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom"
+import github from "./../pics/github.png"
+import linkedIn from "./../pics/linkedIn.png"
 
 const Footer = () => {
     return (
       <footer className="footer">
         <h1>Nichole O'Brien</h1>
         <a href="https://github.com/colie31" alt="">
-          GitHub
+          <img src={github} alt="" />
         </a>
         <a href="https://www.linkedin.com/in/nichole-o-brien-38124a1a3/" alt="">
-          Linkedin
+          <img style={{ height: 32 }} src={linkedIn} alt="" />
         </a>
       </footer>
     );
